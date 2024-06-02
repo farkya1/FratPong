0c7be88269ed98578a3b4adcded63dff976ae89f
farkya1 <farkasa1@msu.edu>
2024-06-02 17:22:00 -0400

    spawning for balls in

--- FratPong/Assets/Scripts/PongBall.cs
+++ FratPong/Assets/Scripts/PongBall.cs
@@ -3,36 +3,96 @@ using UnityEngine;
 
 public class PongBall : MonoBehaviour
 {
-    [Tooltip("Boolean stating whether the ball can be")]
-    [SerializeField] private bool canBallInteract = false;
+    //the two states of the ball 
+    //throwing is when the player can interact
+    //in motion is when the ball is moving after the player has interacted
+    public enum ballState { throwing, inMotion}
+
+
+    private ballState pongBallState = ballState.throwing;
+
+    private PongGameManager pongGameManager;
 
     private Rigidbody rigidbodyBall;
 
     private Vector2 touchStartPos;
 
+    private bool startedDragOverBall = false;
+
+    private Vector2 prevTouchPos;
+
+
 
     private void Start()
     {
         rigidbodyBall = GetComponent<Rigidbody>();
+
+        
+    }
+
+    public void Initialize(PongGameManager pongGameManager)
+    {
+        this.pongGameManager = pongGameManager;
+    }
+
+    private void DestroyBall()
+    {
+        pongGameManager.NewBallSpawn();
+        Destroy(this.gameObject);
+        
     }
 
 
 
     private void Update()
     {
-        if (canBallInteract && Input.touchCount > 0)
+        if ((pongBallState == ballState.inMotion && rigidbodyBall.velocity.magnitude < .01f) || transform.position.y < 0)
+        {
+            DestroyBall();
+        }
+
+        if (pongBallState == ballState.throwing && Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
 
-
+            
             if (touch.phase == TouchPhase.Began)
             {
                 touchStartPos = touch.position;
+
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.collider.gameObject == this.gameObject)
+                    {
+                        startedDragOverBall = true;
+                    }
+                }
+
             }
             else if (touch.phase == TouchPhase.Ended)
             {
-                Vector3 flickDirection = touch.position - touchStartPos;
-                rigidbodyBall.AddForce(1, 0, 1, ForceMode.Impulse);
+                if (startedDragOverBall)
+                {
+                    Vector3 flickDirection = (touch.position - prevTouchPos);
+                    Debug.Log(flickDirection.magnitude);
+
+                    if (flickDirection.magnitude > 50)
+                    {
+                        rigidbodyBall.AddForce(flickDirection.x/10, 3, flickDirection.magnitude/100, ForceMode.Impulse);
+                        rigidbodyBall.useGravity = true;
+                        pongBallState = ballState.inMotion;
+                    }
+                    
+
+                }
+                startedDragOverBall = false;
+            }
+            else
+            {
+                prevTouchPos = touch.position;
             }
         }
     }
--- FratPong/Assets/Scripts/PongGameManager.cs
+++ FratPong/Assets/Scripts/PongGameManager.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 public class PongGameManager : MonoBehaviour
 {
     [Tooltip("The score the player must get to for the win")]
-    [SerializeField]private int winScore = 5;
+    [SerializeField] private int winScore = 5;
+
+    [Tooltip("The number of balls the player can throw each turn")]
+    [SerializeField] private int ballThrowAmount = 2;
+
+    //the number of balls thrown this turn
+    private int currBallThrownAmount = 0;
 
     [Tooltip("Location where the ball will be thrown")]
-    [SerializeField] private GameObject pongBallSpawnLocation;
+    [SerializeField] private Transform pongBallSpawnLocation;
 
-    [Tooltip("The gameobject that will spawn the ball")]
-    [SerializeField] private GameObject pongBall;
+    [Tooltip("The prefab that will spawn the ball")]
+    [SerializeField] private GameObject pongBallPrefab;
+
+    //the current pong ball in play
+    private GameObject currPongBall;
+
+    private void Start()
+    {
+        NewBallSpawn();
+    }
+
+    public void NewBallSpawn()
+    {
+     
+        GameObject pongBallGO = Instantiate(pongBallPrefab, pongBallSpawnLocation.position, Quaternion.identity);
+        PongBall pongBallScript = pongBallGO.GetComponent<PongBall>();
+        pongBallScript.Initialize(this);
+    }
 
     
 }
